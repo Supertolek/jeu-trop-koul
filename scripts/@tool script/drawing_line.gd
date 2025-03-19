a26138268ab341079b8a5dc133f962b5d3a97d0b
Supertolek <supertolekmc@gmail.com>
2025-03-19 13:29:00 +0100

    Refactor drawing_line script for improved camera preview functionality and remove unused tilemap asset

--- scripts/@tool script/drawing_line.gd
+++ scripts/@tool script/drawing_line.gd
@@ -1,72 +1,28 @@
 @tool
+class_name CameraSnapPreview
 extends Node2D
-@export_category('Drawing Line')
-@export_group('Link')
-@export var camera_scene: RoomCamera
-@export var tilemaplayer_node: Array[TileMapLayer]
 
-@export_group('Display')
-@export var display_line: bool = false
+@export_category("Preview settings")
+@export_group("Camera")
+@export var room_size: Vector2i = Vector2i(36, 20) ## Size in tiles displayed
+@export var source_tilemap: TileMapLayer = TileMapLayer.new()
 
-@export var main_rect_color: Color = Color("000000")
-@export var secondary_rect_color: Color = Color("5b5b5b")
+@export_group("Display")
+@export var primary_lines: Color = Color(0, 0, 0)
+@export var secondary_lines: Color
+@export var rooms_around: int = 2
 
-@export_range(0,20,0.1,'or_greater') var main_rect_width: float = 10
-@export_range(0,20,0.1,'or_greater') var secondary_rect_width: float = 5
-
-
-
+var editor_settings: EditorSettings = EditorInterface.get_editor_settings()
 
 func _process(delta: float) -> void:
 	if position:
 		position = Vector2.ZERO
 	queue_redraw()
 
-
-func get_camera_center_based_on_mouse_global_pos():
-	var offset_size = camera_scene.offset_size
-	var screen_size = Vector2(
-	ProjectSettings.get_setting("display/window/size/viewport_width"),
-	ProjectSettings.get_setting("display/window/size/viewport_height"),
-	)
-	var mouse_pos = get_global_mouse_position()
-	var x = floor((mouse_pos.x) / (screen_size.x - 2*offset_size)) * (screen_size.x - 2*offset_size) + (screen_size.x - 2*offset_size)/2
-	var y = floor((mouse_pos.y) / (screen_size.y - 2*offset_size)) * (screen_size.y - 2*offset_size) + (screen_size.y - 2*offset_size)/2
-	return Vector2(x,y)
-	
-
 func _draw() -> void:
+	var camera_size := Vector2(room_size) * source_tilemap.scale * 32
 	
-	if camera_scene and display_line:
-		var offset_size = camera_scene.offset_size
-		var screen_center: Vector2 = get_camera_center_based_on_mouse_global_pos()
-		var screen_size = Vector2(
-		ProjectSettings.get_setting("display/window/size/viewport_width"),
-		ProjectSettings.get_setting("display/window/size/viewport_height"),
-		)
-		for i in [
-			Vector2(-1,-1),Vector2(0,-1),Vector2(1,-1),
-			Vector2(-1,0),Vector2(1,0),
-			Vector2(-1,1),Vector2(0,1),Vector2(1,1),
-			Vector2(0,0),
-		]:
-			var screen_rect_center: Vector2 = Vector2(screen_center.x + i.x * screen_size.x - 2 * i.x * offset_size, screen_center.y + i.y * screen_size.y - 2 * i.y * offset_size)
-			var points:PackedVector2Array = PackedVector2Array([
-				Vector2(screen_rect_center.x + (screen_size.x - 2*offset_size)/2,screen_rect_center.y + (screen_size.y - 2*offset_size)/2),
-				Vector2(screen_rect_center.x - (screen_size.x - 2*offset_size)/2,screen_rect_center.y + (screen_size.y - 2*offset_size)/2),
-				Vector2(screen_rect_center.x - (screen_size.x - 2*offset_size)/2,screen_rect_center.y - (screen_size.y - 2*offset_size)/2),
-				Vector2(screen_rect_center.x + (screen_size.x - 2*offset_size)/2,screen_rect_center.y - (screen_size.y - 2*offset_size)/2),
-				Vector2(screen_rect_center.x + (screen_size.x - 2*offset_size)/2,screen_rect_center.y + (screen_size.y - 2*offset_size)/2)
-			])
-			var color: Color 
-			var width: float
-			if i == Vector2(0,0):
-				width = main_rect_width
-				color = main_rect_color
-			else: 
-				width = secondary_rect_width
-				color = secondary_rect_color
-		
-		
-			draw_polyline(points,color,width)
+	var mouse_pos := get_global_mouse_position()
+	var clamped_mouse_pos = round(mouse_pos/camera_size) * camera_size
 	
+	draw_rect(Rect2(clamped_mouse_pos - camera_size/2, camera_size), editor_settings.get_setting("interface/theme/accent_color"), false)
