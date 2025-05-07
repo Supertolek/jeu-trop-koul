df19186bea890e5be9203105e3df57bb2a801529
TyAltern <tyalternative3333@gmail.com>
2025-05-08 01:32:03 +0200

    Added Key, Stars, Coins, Doors, Vase, Create, Padlocks and Spike to the game

---
+++ scripts/global_scripts/GameController.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+func coin_collected(coin_color:GlobalObjectsMgmt.COIN_COLOR):
+	SignalBus.emit_signal("coin_collected", coin_color)
+
+func key_collected(key_color: GlobalObjectsMgmt.KEY_COLOR):
+	SignalBus.emit_signal("key_collected",key_color)
+	
+func star_collected(star_color: GlobalObjectsMgmt.STAR_COLOR):
+	SignalBus.emit_signal("star_collected",star_color)
---
+++ scripts/global_scripts/SignalBus.gd
@@ -0,0 +1,5 @@
+extends Node
+
+signal coin_collected(coin_color:GlobalObjectsMgmt.COIN_COLOR)
+signal key_collected(key_color:GlobalObjectsMgmt.KEY_COLOR)
+signal star_collected(star_color:GlobalObjectsMgmt.STAR_COLOR)
