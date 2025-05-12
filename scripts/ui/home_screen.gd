e7f4d698160d0fe9a39732b88119b4a4c4b88c43
Supertolek <supertolekmc@gmail.com>
2025-05-12 19:43:56 +0200

    Added second play button with disabled state and updated connection for launching game (thanks to copilot for this message)

--- scripts/ui/home_screen.gd
+++ scripts/ui/home_screen.gd
@@ -4,7 +4,3 @@ func launch_game() -> void:
 	var game_scene = GameManager.load_game(1000)
 	get_tree().root.add_child(game_scene)
 	queue_free()
-
-func _init() -> void:
-	#%PlayButton.connect("pressed", launch_game)
-	pass
